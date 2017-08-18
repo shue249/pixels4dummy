426679aa74e0dc8995cde15d4308f2be1b865c81
Anish Bhayani <abhayani@fb.com>
2017-08-18 14:58:28 -0700

    event params

--- events.js
+++ events.js
@@ -52,14 +52,37 @@ function addEventsToMenu() {
   Object.keys(EVENTS).forEach(function (key) {
     var event = EVENTS[key];
     var option = document.createElement("option");
-    console.log(event);
     option.text = event['name'];
     option.value = key;
     menu.appendChild(option);
   });
-}    
+}
+
+function onEventSelect() {
+  var menu = document.getElementById("event-selector");
+  var paramsGroupDiv = document.getElementById("pixel-params-group");
+  var paramsDiv = document.getElementById("pixel-params");
+  var selectedEvent = menu.options[menu.selectedIndex].value;
+  var list = document.createElement('ul');
+
+  Object.values(EVENTS[selectedEvent]['parameters']).forEach(function (param) {
+    var item = document.createElement('li');
+    if (EVENTS[selectedEvent]['required'].includes(param)) {
+      item.appendChild(document.createTextNode(param + " (required)"));
+    } else {
+      item.appendChild(document.createTextNode(param));
+    }
+    list.appendChild(item);
+  });
+  paramsDiv.innerHTML = '';
+  paramsDiv.appendChild(list);
+  paramsGroupDiv.style.display = 'block';
+}
 
 function init() {
   addEventsToMenu();
+
+  var menu = document.getElementById("event-selector");
+  menu.addEventListener('change', onEventSelect, false);
 }    
 document.addEventListener('DOMContentLoaded', init);

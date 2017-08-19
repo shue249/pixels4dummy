3051643fffacc3ab849857c3e6bd9f2d7a6c1362
Chia Shue Ching <shueching@fb.com>
2017-08-18 18:48:50 -0700

    pixel code

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
-var pixelLayer = {
-	content_ids: 'product_id',
-	value: 'price'
+var pixelLayer = {};
+
+var EVENTS = {
+  'ViewContent': {
+    'name': 'ViewContent',
+    'parameters': ['value', 'currency', 'content_name', 'content_type', 'content_ids'],
+    'required': []
+  },
+  'Search': {
+    'name': 'Search',
+    'parameters': ['value', 'currency', 'content_category', 'content_ids', 'search_string'],
+    'required': []
+  },
+  'AddToCart': {
+    'name': 'AddToCart',
+    'parameters': ['value', 'currency', 'content_name', 'content_type', 'content_ids'],
+    'required': []
+  },
+  'AddToWishlist': {
+    'name': 'AddToWishlist',
+    'parameters': ['value', 'currency', 'content_name', 'content_category', 'content_ids'],
+    'required': []
+  },
+  'InitiateCheckout': {
+    'name': 'InitiateCheckout',
+    'parameters': ['value', 'currency', 'content_name', 'content_category', 'content_ids', 'num_items'],
+    'required': []
+  },
+  'AddPaymentInfo': {
+    'name': 'AddPaymentInfo',
+    'parameters': ['value', 'currency', 'content_category', 'content_ids'],
+    'required': []
+  },
+  'Purchase': {
+    'name': 'Purchase',
+    'parameters': ['value', 'currency', 'content_name', 'content_type', 'content_ids', 'num_items'],
+    'required': ['value', 'currency']
+  },
+  'Lead': {
+    'name': 'Lead',
+    'parameters': ['value', 'currency', 'content_name', 'content_category'],
+    'required': []
+  },
+  'CompleteRegistration': {
+    'name': 'CompleteRegistration',
+    'parameters': ['value', 'currency', 'content_name', 'status'],
+    'required': []
+  }
 };
 
 function log(obj) {
@@ -8,7 +53,7 @@ function log(obj) {
   chrome.devtools.inspectedWindow.eval('console.log(' + str + ');');
 }
 
-function getSelectedID() {
+function getSelectedID(link) {
   var selectedID;
   chrome.devtools.inspectedWindow.eval('$0.id',
   function(res, err) {
@@ -24,24 +69,20 @@ function getSelectedID() {
 }
 
 function getCode() {
-  log('start getcode');
-  log(pixelLayer.content_ids);  
+  log('start getcode');  
+
   var eventName = document.getElementById('event-selector').value;
   var contentType = 'product';
+  var content_ids = 'product_id';
+  var value = 'price';
   var mode = 'pageload';
   var codeSnippet = document.getElementById('code-snippet');
-  var generatedCode = generateCode(eventName, contentType, pixelLayer.content_ids, pixelLayer.value, mode);
+  var generatedCode = generateCode(eventName, contentType, content_ids, value, mode);
   log(generatedCode);
   codeSnippet.append(generatedCode);
   show('code-snippet-block');
   log('end getcode');
 }
-
-function init() {
-  document.getElementById('getcode').addEventListener('click', function() {getCode();});
-  getSelectedID();
-}
-document.addEventListener('DOMContentLoaded', init);
 
 // eventName - name of the pixel event, string, possible values are ViewContent, AddToCart, Purchase
 // contentIdsElement - id or name of the element containing the content_ids, json, eg {id: 'product_id'} or {name: 'product_id'}
@@ -68,11 +109,11 @@ function generateCode(eventName, contentType, contentIdsElement, valueElement, m
 }
 
 function generateCodeForObject(element) {	
-	if (element.id) {
-		return "obj = document.getElementById('" + element.id + "');";
-	} else {
-		return "obj = document.getElementsByName('" + element.name + "')[0];";
-	}
+	// if (element.id) {
+		return "obj = document.getElementById('" + element + "');";
+	// } else {
+	// 	return "obj = document.getElementsByName('" + element.name + "')[0];";
+	// }
 }
 
 function generateCodeForContentIds(element) {
@@ -133,3 +174,60 @@ function show(id) {
     element.style.display = 'block';
   }
 }
+
+
+
+function addEventsToMenu() {
+  var menu = document.getElementById("event-selector");
+
+  Object.keys(EVENTS).forEach(function (key) {
+    var event = EVENTS[key];
+    var option = document.createElement("option");
+    option.text = event['name'];
+    option.value = key;
+    menu.appendChild(option);
+  });
+}
+
+function attributeIDtoLink(link) {
+	var id = getSelectedID();
+	var text = link.innerHTML;
+	pixelLayer.push({text: id});
+	link.innerHTML.append(': ' + id);
+}
+
+function onEventSelect() {
+  var menu = document.getElementById("event-selector");
+  var paramsGroupDiv = document.getElementById("pixel-params-group");
+  var paramsDiv = document.getElementById("pixel-params");
+  var selectedEvent = menu.options[menu.selectedIndex].value;
+  var list = document.createElement('ul');
+
+  Object.values(EVENTS[selectedEvent]['parameters']).forEach(function (param) {
+    var item = document.createElement('li');
+    var link = document.createElement('a');
+		link.setAttribute('href', '#');
+		link.setAttribute('id', selectedEvent.toString() + '_' + param);
+		link.addEventListener('click', () => attributeIDtoLink(this));
+    if (EVENTS[selectedEvent]['required'].includes(param)) {
+      link.appendChild(document.createTextNode(param + " (required)"));
+    } else {
+      link.appendChild(document.createTextNode(param));
+    }
+		item.appendChild(link);
+    list.appendChild(item);
+  });
+  paramsDiv.innerHTML = '';
+  paramsDiv.appendChild(list);
+  paramsGroupDiv.style.display = 'block';
+}
+
+function init() {
+  document.getElementById('getcode').addEventListener('click', function() {getCode();});
+  getSelectedID();
+
+  addEventsToMenu();
+  var menu = document.getElementById("event-selector");
+  menu.addEventListener('change', onEventSelect, false);
+}    
+document.addEventListener('DOMContentLoaded', init);
